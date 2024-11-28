e26a5ee880150f72b8adfc7b80909741731b0b65
ArtemRudomanenko <artemrudomanenko@gmail.com>
2024-11-28 22:55:27 +0200

    GOTOVO HOROSHO

--- 2ists/2ists.cpp
+++ 2ists/2ists.cpp
@@ -1,39 +1,26 @@
-// 2lists.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include "List.h"
 using namespace std;
 
-bool func(int value) {
-	return value> 5;
+int main() {
+    List<int> list1;
+    list1.push_back(1);
+    list1.push_back(2);
+    list1.push_back(3);
+
+    List<int> list2;
+    list2.push_back(2);
+
+    List<int> list3 = list1 + list2;
+    List<int> list4 = list1 * list2;
+
+    list1.show();
+
+    list2.show();
+
+    list3.show();
+
+    list4.show();
+
+    return 0;
 }
-
-bool func1(int value) {
-	return value < 5;
-}
-
-int main()
-{
-	List list;
-	list.push_back(10);
-	list.push_back(12);
-	list.push_back(13);
-	List list2;
-	list2.push_back(1);
-	list2.push_back(2);
-	list2.push_back(3);
-	List list3 = list + list2;
-	list3.show();
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- 2ists/List.h
+++ 2ists/List.h
@@ -1,55 +1,144 @@
 #pragma once
+#include <iostream>
+using namespace std;
 
-class List
-{
-	class Node {
+template <typename T>
+class List {
+    class Node {
+    public:
+        T value;
+        Node* prev;
+        Node* next;
+        Node(T value) : value(value), prev(nullptr), next(nullptr) {}
+        Node(T value, Node* prev) : value(value), prev(prev), next(nullptr) {}
+        Node(Node* next, T value) : value(value), prev(nullptr), next(next) {}
+    };
 
-	public:
-		int value;
-		Node* prev;
-		Node* next;
-		Node() {
-			this->value = 0;
-			prev = next = nullptr;
-		}
-		Node(int value) {
-			this->value = value;
-			prev = next = nullptr;
-		}
-		Node(int value, Node* prev) {
-			this->value = value;
-			this->prev = prev;
-			next = nullptr;
-		}
-		Node(Node* next, int value) {
-			this->value = value;
-			this->next = next;
-			prev = nullptr;
-		}
-
-	};
 private:
-	Node* head;
-	Node* tail;
-	size_t size;
+    Node* head;
+    Node* tail;
+    size_t size;
+
 public:
-	List();
-	void push_back(int v);
-	void push_front(int v);
-	List(const List& obj);
-	~List();
-	void operator=(const List& obj);
-	int operator[](int i);
-	bool insert(int index,int value);
-	int replace(int key,int value);
-	int replace(bool(*predicate)(int value), int key, int value);
-	int back()const;
-	int front()const;
-	void show() const;
-	void pop_back();
-	void pop_front();
-	void reverse();
-	List& operator+(List&obj);
-	List* operator*(List& obj);
-	void clear();
+    List() : head(nullptr), tail(nullptr), size(0) {}
+
+    ~List() { clear(); }
+
+    void push_back(T value) {
+        if (tail) {
+            tail->next = new Node(value, tail);
+            tail = tail->next;
+        }
+        else {
+            head = tail = new Node(value);
+        }
+        size++;
+    }
+
+    void push_front(T value) {
+        if (head) {
+            head = new Node(head, value);
+            head->next->prev = head;
+        }
+        else {
+            head = tail = new Node(value);
+        }
+        size++;
+    }
+
+    void pop_back() {
+        if (!tail) return;
+        Node* to_delete = tail;
+        tail = tail->prev;
+        if (tail) tail->next = nullptr;
+        else head = nullptr;
+        delete to_delete;
+        size--;
+    }
+
+    void pop_front() {
+        if (!head) return;
+        Node* to_delete = head;
+        head = head->next;
+        if (head) head->prev = nullptr;
+        else tail = nullptr;
+        delete to_delete;
+        size--;
+    }
+
+    void clear() {
+        while (head) pop_front();
+    }
+
+    void show() const {
+        Node* current = head;
+        while (current) {
+            cout << current->value << '\t';
+            current = current->next;
+        }
+        cout << endl;
+    }
+
+    T front() const { return head ? head->value : T(); }
+    T back() const { return tail ? tail->value : T(); }
+
+    List(const List& other) : head(nullptr), tail(nullptr), size(0) {
+        Node* current = other.head;
+        while (current) {
+            push_back(current->value);
+            current = current->next;
+        }
+    }
+
+    List& operator=(const List& other) {
+        if (this != &other) {
+            clear();
+            Node* current = other.head;
+            while (current) {
+                push_back(current->value);
+                current = current->next;
+            }
+        }
+        return *this;
+    }
+
+    T operator[](int index) const {
+        Node* current = head;
+        for (int i = 0; i < index && current; ++i) {
+            current = current->next;
+        }
+        return current ? current->value : T();
+    }
+
+    List operator+(const List& other) const {
+        List result;
+        Node* current = this->head;
+        while (current) {
+            result.push_back(current->value);
+            current = current->next;
+        }
+        current = other.head;
+        while (current) {
+            result.push_back(current->value);
+            current = current->next;
+        }
+        return result;
+    }
+
+    List operator*(const List& other) const {
+        List result;
+        Node* current = this->head;
+        while (current) {
+            Node* other_current = other.head;
+            while (other_current) {
+                if (current->value == other_current->value) {
+                    result.push_back(current->value);
+                    break;
+                }
+                other_current = other_current->next;
+            }
+            current = current->next;
+        }
+        return result;
+    }
 };
